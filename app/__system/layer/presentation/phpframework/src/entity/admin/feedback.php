78f31bb2e1a7acc917e1e90af12624857b2be309
a19836 <a19836@gmail.com>
2025-05-08 15:21:41 +0100

    optimizing

--- app/__system/layer/presentation/phpframework/src/entity/admin/feedback.php
+++ app/__system/layer/presentation/phpframework/src/entity/admin/feedback.php
@@ -5,8 +5,10 @@ $popup = isset($_GET["popup"]) ? $_GET["popup"] : null;
 $status = isset($_GET["status"]) ? $_GET["status"] : null;
 $msg = isset($_GET["msg"]) ? $_GET["msg"] : null;
 
-if ($status)
-	$status_message = $msg ? $msg : "Feedback sent successfully!";
-else
-	$error_message = $msg ? $msg : "Feedback not sent! Please try again...";
+if (isset($status)) {
+	if ($status)
+		$status_message = $msg ? $msg : "Feedback sent successfully!";
+	else
+		$error_message = $msg ? $msg : "Feedback not sent! Please try again...";
+}
 ?>
